8b374d6261902471e803d1277889b65a12012367
karen <karen@karen>
2019-09-12 18:44:15 -0400

    añadiendo el segundo get mapping

--- Spring/src/main/java/com/example/demo/MinibusController.java
+++ Spring/src/main/java/com/example/demo/MinibusController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import java.util.List;
 
+import org.springframework.hateoas.Resource;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.karen_velasquez.www.ProGit.Minibus;
+import com.karen_velasquez.www.ProGit.MinibusController;
+import com.karen_velasquez.www.ProGit.MinibusNotFoundException;
 
 @RestController
 class MinibusController {
@@ -20,7 +25,6 @@ class MinibusController {
   }
 
   // Aggregate root
-
   @GetMapping("/minibuses")
   List<Minibus> all() {
     return repository.findAll();
@@ -34,11 +38,32 @@ class MinibusController {
   // Single item
 
   @GetMapping("/minibuses/{id}")
+  Resource<Minibus> two(@PathVariable Long id) {
+    Minibus minibus = repository.findById(id)
+      .orElseThrow(() -> new MinibusNotFoundException(id));
+    return new Resource<>(minibus,
+      linkTo(methodOn(MinibusController.class).two(id)).withSelfRel(),
+      linkTo(methodOn(MinibusController.class).all()).withRel("minibuses"));
+  }
+  
+ 
+  @GetMapping("/employees/{id}")
+  Resource<Minibus> three(@PathVariable Long id) {
+
+    Minibus minibus = repository.findById(id)
+      .orElseThrow(() -> new MinibusNotFoundException(id));
+
+    return new Resource<>(minibus,
+      linkTo(methodOn(MinibusController.class).three(id)).withSelfRel(),
+      linkTo(methodOn(MinibusController.class).all()).withRel("employees"));
+  }
+  @GetMapping("/minibuses/{id}")
   Minibus one(@PathVariable Long id) {
 
     return repository.findById(id)
       .orElseThrow(() -> new MinibusNotFoundException(id));
   }
+  
 
   @PutMapping("/minibuses/{id}")
   Minibus replaceMinibus(@RequestBody Minibus newMinibus, @PathVariable Long id) {
